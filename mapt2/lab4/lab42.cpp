1ebe239cbf424e10cf2a443b815b6aecd85caef2
Kamil Chaj <kamilchaj@student.agh.edu.pl>
2023-04-02 00:55:59 +0200

    lab4, report

--- mapt2/lab4/lab42.cpp
+++ mapt2/lab4/lab42.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <queue>
 
-void push_limited_queue(std::queue<int> q, int limit, int value)
+bool push_limited_queue(std::queue<int>& q, int limit, int value)
 {
     if(q.size() >= limit)
     {
-        std::cout<<"queue is full!\n";
-        return;
+        return true;
     }
     q.push(value);
+    return false;
 }
 
 void display(std::queue<int>& q)
@@ -19,16 +19,57 @@ void display(std::queue<int>& q)
         std::cout<<q.front()<<" ";
         q.pop();
     }
+    std::cout<<"\n";
+}
+
+void pop_n_elements(std::queue<int>& q, int n)
+{
+    if(q.size() < n) { std::cout<<"queue is too small"; return; }
+    for(int i = 0; i < n; i++)
+    {
+        q.pop();
+    }
+    std::cout<<"ereased "<<n<<" elements\n";
+}
+
+void populate_queue(std::queue<int>& q,int first_element, int step, int last_element)
+{
+    int i = first_element;
+    for(; i <= last_element; i+=step)
+    {
+        q.push(i);
+    }
+    std::cout<<"populated queue with "<< (i-first_element)/step << " elements\n";
+}
+
+void populate_queue(std::queue<int>& q, int first_element, int step, int last_element, int limit)
+{
+    bool limit_reached = false;
+    int i = first_element;
+    for(; i <= last_element; i+=step)
+    {
+        limit_reached = push_limited_queue(q, limit, i);
+        if(limit_reached)
+        {
+            std::cout<<"queue limit exeeded by "<< (i - first_element)/step 
+                << " elements\n" << "all element over the limit will be erased\n";
+            return;
+        }
+    }
+    std::cout<<"populated queue with "<< (i-first_element)/step << " elements\n";
 }
 
 int main()
 {
     std::queue<int> q;
-    for(int i = 0; i < 12; i++)
-    {
-        q.push(i);
-    }
+    populate_queue(q, 1, 1, 12);
     display(q);
     std::string s = q.empty() ? "queue is empty!\n" : "queue isn't empty";
-    std::cout<< "\n" << s ;
+    std::cout<< s ;
+    populate_queue(q, 1, 5, 55);
+    pop_n_elements(q, 5);
+    std::cout<<"length of the queue "<<q.size()<<" \n";
+    display(q);
+    populate_queue(q, 1, 1, 4, 3);
+    display(q);
 }
